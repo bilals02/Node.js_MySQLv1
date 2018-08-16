cfef1d11cd149f6c04aa1f66bf41631519c11c61
Bilal S <bilals@users-MacBook-Pro.local>
2018-08-16 02:56:29 -0400

    updating files

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,61 +1,95 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer");
+//require mysql and inquirer
+var mysql = require('mysql');
+var inquirer = require('inquirer');
 
-
+//create connection to db
 var connection = mysql.createConnection({
   host: "localhost",
-
-  // Your port; if not 3306
   port: 3306,
-
-  // Your username
   user: "root",
-
-  // Your password
   password: "bilal2018",
   database: "bamazon"
-});
+})
 
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId);
+function start(){
+//prints the items for sale and their details
+connection.query('SELECT * FROM Products', function(err, res){
+  if(err) throw err;
 
-  connection.query("SELECT * FROM products", function(err, response){
-    var data=response;
+  console.log('****Welcome to Bamazon****')
+  console.log('----------------------------------------------------------------------------------------------------')
 
-    for (i=0;i<data.length;i++){
-        console.log("\nITEM FOR SALE:");
-        console.log("------------");
-        console.log("Item ID: " + data[i].item_id);
-        console.log("Product Name: " + data[i].product_name);
-        console.log("Department Name: " + data[i].price);
-        console.log("Stock Qty: " + data[i].stock_quantity);
-    }
-
-
-    if(err) throw err;
-});
-
-
-connection.end();
-
-
-function findProduct() {
-    inquirer
-      .prompt({
-        name: "product",
-        type: "input",
-        message: "What artist would you like to search for?"
-      })
-      .then(function(answer) {
-        var query = "SELECT position, song, year FROM top5000 WHERE ?";
-        connection.query(query, { artist: answer.artist }, function(err, res) {
-          for (var i = 0; i < res.length; i++) {
-            console.log("Position: " + res[i].position + " || Song: " + res[i].song + " || Year: " + res[i].year);
-          }
-          runSearch();
-        });
-      });
+  for(var i = 0; i<res.length;i++){
+    console.log("ID: " + res[i].item_id + " | " + "Product: " + res[i].product_name + " | " + "Department: " + res[i].department_name + " | " + "Price: " + res[i].price + " | " + "QTY: " + res[i].stock_quantity);
+    console.log('--------------------------------------------------------------------------------------------------')
   }
 
-});
+  console.log(' ');
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "id",
+      message: "What is the ID of the product you would like to purchase?",
+      validate: function(value){
+        if(isNaN(value) == false && parseInt(value) <= res.length && parseInt(value) > 0){
+          return true;
+        } else{
+          return false;
+        }
+      }
+    },
+    {
+      type: "input",
+      name: "qty",
+      message: "How much would you like to purchase?",
+      validate: function(value){
+        if(isNaN(value)){
+          return false;
+        } else{
+          return true;
+        }
+      }
+    }
+    ]).then(function(ans){
+      var whatToBuy = (ans.id)-1;
+      var howMuchToBuy = parseInt(ans.qty);
+      var grandTotal = parseFloat(((res[whatToBuy].price)*howMuchToBuy).toFixed(2));
+
+      //check if quantity is sufficient
+      if(res[whatToBuy].stock_quantity >= howMuchToBuy){
+        //after purchase, updates quantity in Products
+        connection.query("UPDATE products SET ? WHERE ?", [
+        {stock_quantity: (res[whatToBuy].stock_quantity - howMuchToBuy)},
+        {item_id: ans.id}
+        ], function(err, result){
+            if(err) throw err;
+            console.log("Success! Your total is $" + grandTotal.toFixed(2) + ". Your item(s) will be shipped to you in 3-5 business days.");
+        });
+
+
+      } else{
+        console.log("Sorry, there's not enough in stock!");
+
+      }
+
+      reprompt();
+    })
+})
+}
+
+//asks if they would like to purchase another item
+function reprompt(){
+  inquirer.prompt([{
+    type: "confirm",
+    name: "reply",
+    message: "Would you like to purchase another item?"
+  }]).then(function(ans){
+    if(ans.reply){
+      start();
+    } else{
+      console.log("See you soon!");
+    }
+  });
+}
+
+start();
